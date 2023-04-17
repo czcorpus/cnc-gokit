d0446af30a9ee2c17f21d715f263b4aaf8fed606
Martin Zimandl <martin.zimandl@gmail.com>
2023-04-17 17:14:11 +0200

    add missing fs functions

--- fs/main.go
+++ fs/main.go
@@ -17,7 +17,9 @@ package fs
 
 import (
 	"fmt"
+	"io/ioutil"
 	"os"
+	"sort"
 	"time"
 )
 
@@ -74,4 +76,89 @@ func IsDir(path string) (bool, error) {
 		return false, err
 	}
 	return finfo.Mode().IsDir(), nil
+}
+
+// PathExists tests whether  the provided path exists no matter what it
+// is (file, dir, ...)
+func PathExists(path string) bool {
+	_, err := os.Stat(path)
+	return !os.IsNotExist(err)
+}
+
+// FileSize returns size of a provided file.
+// In case of an error the function returns -1 and logs the error.
+func FileSize(path string) (int64, error) {
+	f, err := os.Open(path)
+	if err != nil {
+		return 0, err
+	}
+	defer f.Close()
+	finfo, err := f.Stat()
+	if err != nil {
+		return 0, err
+	}
+	return finfo.Size(), nil
+}
+
+// FileList is an abstraction for list of files along with their
+// modification time information. It supports sorting.
+type FileList struct {
+	files []os.FileInfo
+}
+
+func (f *FileList) Len() int {
+	return len(f.files)
+}
+
+func (f *FileList) Less(i, j int) bool {
+	return f.files[i].ModTime().After(f.files[j].ModTime())
+}
+
+func (f *FileList) Swap(i, j int) {
+	f.files[i], f.files[j] = f.files[j], f.files[i]
+}
+
+// First returns an item with the latest modification time.
+func (f *FileList) First() os.FileInfo {
+	return f.files[0]
+}
+
+func (f *FileList) ForEach(fn func(info os.FileInfo, idx int) bool) {
+	for i, v := range f.files {
+		if !fn(v, i) {
+			break
+		}
+	}
+}
+
+// ListFilesInDir lists files according to their modification time
+// (newest first).
+func ListFilesInDir(path string, newestFirst bool) (FileList, error) {
+	var ans FileList
+	files, err := ioutil.ReadDir(path)
+	if err != nil {
+		return ans, err
+	}
+	ans.files = make([]os.FileInfo, len(files))
+	copy(ans.files, files)
+	if newestFirst {
+		sort.Sort(&ans)
+	}
+	return ans, nil
+}
+
+// ListDirsInDir lists directories in a directory (without recursion).
+func ListDirsInDir(path string, newestFirst bool) (FileList, error) {
+	var ans FileList
+	files, err := ioutil.ReadDir(path)
+	if err != nil {
+		return ans, err
+	}
+	ans.files = make([]os.FileInfo, 0, 200)
+	for _, v := range files {
+		if v.IsDir() {
+			ans.files = append(ans.files, v)
+		}
+	}
+	return ans, nil
 }
