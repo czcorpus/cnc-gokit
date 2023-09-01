f2713441d597f97f4a38544a74e5c3908b8a0303
Tomas Machalek <tomas@machalek.xyz>
2023-09-01 09:48:21 +0200

    Add GetURLBoolArgOrFail, GetURLIntArgOrFail

--- unireq/request.go
+++ unireq/request.go
@@ -21,8 +21,11 @@ import (
 	"fmt"
 	"net"
 	"net/http"
+	"strconv"
 
 	"github.com/czcorpus/cnc-gokit/collections"
+	"github.com/czcorpus/cnc-gokit/uniresp"
+	"github.com/gin-gonic/gin"
 )
 
 // CheckSuperfluousURLArgs allows checking for presence of supported only
@@ -55,4 +58,53 @@ func ClientIP(req *http.Request) net.IP {
 		return net.ParseIP(src)
 	}
 	return net.ParseIP(req.RemoteAddr)
+}
+
+// GetURLIntArgOrFail reads a string-encoded integer argument from URL query.
+// If not set, then `dflt` is returned (i.e. value not present is considered a non-error).
+// The second returned value is an "OK" flag.
+// In case of an error, the function writes a HTTP response and returns
+// false as a second argument.
+func GetURLIntArgOrFail(ctx *gin.Context, name string, dflt int) (int, bool) {
+	if !ctx.Request.URL.Query().Has(name) {
+		return dflt, true
+	}
+	tmp := ctx.Request.URL.Query().Get(name)
+	value, err := strconv.Atoi(tmp)
+	if err != nil {
+		uniresp.WriteJSONErrorResponse(
+			ctx.Writer,
+			uniresp.NewActionErrorFrom(err),
+			http.StatusUnprocessableEntity,
+		)
+		return 0, false
+	}
+	return value, true
+}
+
+// GetURLBoolArgOrFail reads a string-encoded bool argument (= '1', '0') from URL query.
+// If not set, then `dflt` is returned (i.e. value not present is considered a non-error).
+// The second returned value is an "OK" flag.
+// In case of an error, the function writes a HTTP response and returns
+// false as a second argument.
+func GetURLBoolArgOrFail(ctx *gin.Context, name string, dflt bool) (bool, bool) {
+	if !ctx.Request.URL.Query().Has(name) {
+		return dflt, true
+	}
+	tmp := ctx.Request.URL.Query().Get(name)
+	if tmp == "0" {
+		return false, true
+
+	} else if tmp == "1" {
+		return true, true
+
+	} else {
+		err := fmt.Errorf("invalid URL bool value: %s", tmp)
+		uniresp.WriteJSONErrorResponse(
+			ctx.Writer,
+			uniresp.NewActionErrorFrom(err),
+			http.StatusUnprocessableEntity,
+		)
+		return false, false
+	}
 }
