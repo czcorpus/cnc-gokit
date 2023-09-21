72ab495bc0cc1beaf37751bd7208dac648f8c980
Tomas Machalek <tomas@machalek.xyz>
2023-09-21 17:49:23 +0200

    MapUpdate function

---
+++ collections/map.go
@@ -0,0 +1,23 @@
+// Copyright 2022 Tomas Machalek <tomas.machalek@gmail.com>
+// Copyright 2022 Institute of the Czech National Corpus,
+//                Faculty of Arts, Charles University
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package collections
+
+func MapUpdate[K comparable, V any](curr map[K]V, incom map[K]V) {
+	for k, v := range incom {
+		curr[k] = v
+	}
+}
