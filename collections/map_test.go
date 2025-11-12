883846cf4876b98a0466a94be13349cb8593c337
Tomas Machalek <tomas@machalek.xyz>
2025-11-12 13:17:03 +0100

    New functions collections.MapToEntries, collections.MapToEntriesSorted

--- collections/map_test.go
+++ collections/map_test.go
@@ -16,6 +16,7 @@
 package collections
 
 import (
+	"fmt"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -43,4 +44,47 @@ func TestMapUpdateEmpty(t *testing.T) {
 	MapUpdate(m1, m2)
 	assert.Equal(t, map[string]int{"a": 1, "b": 2, "c": 3}, m1)
 	assert.Equal(t, map[string]int{"a": 1, "b": 2, "c": 3}, m2)
+}
+
+func TestMapToEntries(t *testing.T) {
+	m1 := map[string]int{"foo": 10, "bar": 20, "baz": 30}
+	items := MapToEntries(m1)
+	assert.Equal(t, 3, len(items))
+	hits := 0
+	for _, entry := range items {
+		fmt.Println("ENTRY: ", entry)
+		if entry.K == "foo" && entry.V == 10 {
+			hits++
+		}
+		if entry.K == "bar" && entry.V == 20 {
+			hits++
+		}
+		if entry.K == "baz" && entry.V == 30 {
+			hits++
+		}
+	}
+	assert.Equal(t, 3, hits)
+}
+
+func TestMapToEntriesEmpty(t *testing.T) {
+	m1 := map[string]int{}
+	items := MapToEntries(m1)
+	assert.Equal(t, 0, len(items))
+}
+
+func TestMapToEntriesSorted(t *testing.T) {
+	m1 := map[string]int{"foo": 10, "bar": 20, "baz": 30}
+	items := MapToEntriesSorted(m1, func(a, b MapEntry[string, int]) int {
+		if a.V > b.V {
+			return -1
+		}
+		if a.V < b.V {
+			return 1
+		}
+		return 0
+	})
+	assert.Equal(t, 3, len(items))
+	assert.Equal(t, "baz", items[0].K)
+	assert.Equal(t, "bar", items[1].K)
+	assert.Equal(t, "foo", items[2].K)
 }
